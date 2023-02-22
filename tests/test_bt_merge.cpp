ba5e4c6543aa05424c45ecf9be7c50656f680624
Qian Hong <fracting@gmail.com>
2023-02-22 18:38:19 +1100

    Add property tests for session::bt::merge.

--- tests/test_bt_merge.cpp
+++ tests/test_bt_merge.cpp
@@ -48,6 +48,59 @@ TEST_CASE("bt_dict merging", "[bt_dict][merge]") {
     CHECK(session::bt::merge(bt_dict{}, bt_dict{}) == bt_dict{});
 }
 
+namespace session::bt {
+TEST_CASE("bt_dict merging properties", "[bt_dict][merge]") {
+    rc::check("[bt_dict][merge] identity element", [](const bt_dict& x) {
+        auto e = bt_dict{};
+        REQUIRE(merge(e, e) == e);
+        REQUIRE(merge(x, e) == x);
+        REQUIRE(merge(e, x) == x);
+    });
+
+    rc::check(
+            "[bt_dict][merge] singleton dict",
+            [](const bt_dict& x, const std::string& y_k, const bt_value& y_v) {
+                auto y = bt_dict{{y_k, y_v}};
+                auto xy = merge(x, y);
+                auto yx = merge(y, x);
+
+                if (x.count(y_k) > 0) {
+                    REQUIRE(xy.size() == x.size());
+                    REQUIRE(yx.size() == x.size());
+
+                    REQUIRE(xy.at(y_k) == x.at(y_k));
+                    REQUIRE(yx.at(y_k) == y.at(y_k));
+                } else {
+                    REQUIRE(xy.size() == x.size() + 1);
+                    REQUIRE(yx.size() == x.size() + 1);
+
+                    REQUIRE(xy.at(y_k) == y.at(y_k));
+                    REQUIRE(yx.at(y_k) == y.at(y_k));
+                }
+            });
+
+    rc::check("[bt_dict][merge] self merge", [](const bt_dict& x) { REQUIRE(merge(x, x) == x); });
+
+    rc::check(
+            "[bt_dict][merge] associative",
+            [](const bt_dict& x, const bt_dict& y, const bt_dict& z) {
+                REQUIRE(merge(merge(x, y), z) == merge(x, merge(y, z)));
+            });
+
+    rc::check(
+            "[bt_dict][merge] distributive",
+            [](const bt_dict& x, const bt_dict& y, const bt_dict& z) {
+                REQUIRE(merge(x, merge(y, z)) == merge(merge(x, y), merge(x, z)));
+            });
+
+    rc::check("[bt_dict][merge] left priority", [](const bt_dict& x, const bt_dict& y) {
+        auto a = merge(x, y);
+        auto b = merge(y, x);
+        REQUIRE(merge(a, b) == a);
+    });
+}
+}  // namespace session::bt
+
 TEST_CASE("bt_list sorted merge", "[bt_list][merge]") {
     bt_list x{1, 2, 3, 5, 8, 13, 21};
     bt_list y{2, 4, 8, 16};
